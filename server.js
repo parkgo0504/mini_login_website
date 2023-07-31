3ae0a1e8c800b9d73a2feb5717821d4127946f08
park <parkgo0504@naver.com>
2023-07-31 11:33:13 +0900

    server mysql 완료

--- server.js
+++ server.js
@@ -1,37 +1,36 @@
-const oracledb = require('oracledb');
+const mysql = require('mysql2');
 
-// 연결 정보
-const username = 'cgi_5_230721_2';
-const password = 'smhrd2';
-const hostname = 'project-db-cgi.smhrd.com';
-const port = 1524;
-const serviceName = 'xe';
+// MySQL 데이터베이스 연결 설정
+const connection = mysql.createConnection({
+  host: 'localhost', // MySQL 서버 주소
+  user: 'root', // MySQL 사용자 이름
+  password: '12345', // MySQL 비밀번호
+  database: 'my_mini', // 사용할 데이터베이스 이름
+});
 
-// 연결 설정
-const connectionConfig = {
-  user: username,
-  password: password,
-  connectString: `${hostname}:${port}/${serviceName}`
-};
-
-// 데이터베이스 연결
-oracledb.getConnection(connectionConfig, (err, connection) => {
+// MySQL 데이터베이스 연결
+connection.connect((err) => {
   if (err) {
-    console.error('데이터베이스 연결 오류:', err);
+    console.error('MySQL 연결 실패:', err.message);
     return;
   }
+  console.log('MySQL 연결 성공!');
+});
 
-  // 연결 성공 시
-  console.log('데이터베이스에 연결되었습니다.');
+// 간단한 쿼리 실행 예제
+connection.query('SELECT 1 + 1 AS result', (err, results) => {
+  if (err) {
+    console.error('쿼리 실행 실패:', err.message);
+    return;
+  }
+  console.log('쿼리 결과:', results[0].result); // 결과: 2
+});
 
-  // 이후 작업을 진행하거나 쿼리를 실행할 수 있습니다.
-
-  // 연결 종료
-  connection.close((err) => {
-    if (err) {
-      console.error('데이터베이스 연결 종료 오류:', err);
-      return;
-    }
-    console.log('데이터베이스 연결이 종료되었습니다.');
-  });
+// 연결 종료
+connection.end((err) => {
+  if (err) {
+    console.error('MySQL 연결 종료 실패:', err.message);
+    return;
+  }
+  console.log('MySQL 연결 종료!');
 });
